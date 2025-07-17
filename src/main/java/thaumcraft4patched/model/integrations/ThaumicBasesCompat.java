01af4d24b0369991a72e22d89e2bce9727144094
Thenemex <tnmx777@gmail.com>
2025-07-17 09:24:51 +0200

    Added patch for TB Thaumium Bracelet research (missing prereq) .Corrected integration implementation dependency

--- src/main/java/thaumcraft4patched/model/integrations/ThaumicBasesCompat.java
+++ src/main/java/thaumcraft4patched/model/integrations/ThaumicBasesCompat.java
@@ -1,6 +1,10 @@
 package thaumcraft4patched.model.integrations;
 
+import thaumcraft.api.research.ResearchItem;
 import thaumcraft4patched.api.integrations.ACompat;
+import thaumcraft4patched.api.thaumcraft.API;
+import thaumcraft4patched.api.util.exceptions.ParameterIsNullOrEmpty;
+import thaumcraft4patched.config.Config;
 
 public class ThaumicBasesCompat extends ACompat {
 
@@ -10,6 +14,20 @@ public class ThaumicBasesCompat extends ACompat {
 
     @Override
     public void loadIntegration() {
+        if (Config.missingPrereqs_ThaumiumBracelet) addMissingPrereqForThaumiumBracelet();
+    }
 
+    protected void addMissingPrereqForThaumiumBracelet() {
+        ResearchItem research = API.getResearch("THAUMICBASES", "TB.Bracelet.Thaumium");
+        String[] parentsHidden = deepCopyTabAndAdd(research.parentsHidden, "THAUMIUM");
+        research.setParentsHidden(parentsHidden);
+    }
+
+    private String[] deepCopyTabAndAdd(String[] tab, String ... newElements) {
+        if (tab == null || newElements == null || newElements.length == 0) throw new ParameterIsNullOrEmpty();
+        String[] deepCopy = new String[tab.length + newElements.length];
+        System.arraycopy(tab, 0, deepCopy, 0, tab.length);
+        System.arraycopy(newElements, 0, deepCopy, deepCopy.length - newElements.length - 1, newElements.length);
+        return deepCopy;
     }
 }
