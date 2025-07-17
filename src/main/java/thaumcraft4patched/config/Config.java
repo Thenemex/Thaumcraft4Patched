34d6569c9f1f8d5ec98dda9a096cb49d569258c2
Thenemex <tnmx777@gmail.com>
2025-07-17 09:02:06 +0200

    Added three new entries in Config for Thaumic Bases patches

--- src/main/java/thaumcraft4patched/config/Config.java
+++ src/main/java/thaumcraft4patched/config/Config.java
@@ -6,15 +6,14 @@ import java.io.File;
 
 import static thaumcraft4patched.Thaumcraft4Patched.modName;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "ResultOfMethodCallIgnored"})
 public class Config {
 
     protected static Configuration config;
     public static File configDir;
-    public static boolean boneBowResearchPatchEnabled;
-    public static boolean tc4Enabled, tbEnabled;
+    public static boolean tc4Enabled, boneBowResearchPatchEnabled;
+    public static boolean tbEnabled, missingPrereqs_ThaumiumBracelet, missingPrereqs_VoidBracelet, missingPrereqs_VoidWandCore;
 
-    @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void init(File configFolder) {
         configDir = configFolder;
         configDir.mkdirs();
@@ -30,8 +29,11 @@ public class Config {
         tc4Enabled = newEntry(mods, "Thaumcraft 4");
         tbEnabled = newEntry(mods, "Thaumic Bases");
         config.addCustomCategoryComment(tc4, "You can disable/enable bug patches from Thaumcraft 4 here");
-        boneBowResearchPatchEnabled = newEntry(tc4,"HiddenBoneBowResearch", "Remove the hidden property of the research -> it will be unlocked at spawning");
+        boneBowResearchPatchEnabled = newEntry(tc4,"HiddenBoneBowResearch", "Removes the hidden property of the research -> it will be unlocked at spawning");
         config.addCustomCategoryComment(tb, "You can disable/enable bug patches from Thaumic Bases addon here");
+        missingPrereqs_ThaumiumBracelet = newEntry(tb,"MissingPrereqs_ThaumiumBracelet", "Adds the missing prereq(s) for the \"Thaumium Bracelet\" research");
+        missingPrereqs_VoidBracelet = newEntry(tb,"MissingPrereqs_VoidBracelet", "Adds the missing prereq(s) for the \"Void Bracelet\" research");
+        missingPrereqs_VoidWandCore = newEntry(tb, "MissingPrereqs_VoidWandCore", "Adds the missing prereq(s) for the \"Void Wand Core\" research");
     }
 
     public static boolean newEntry(String category, String key) {
