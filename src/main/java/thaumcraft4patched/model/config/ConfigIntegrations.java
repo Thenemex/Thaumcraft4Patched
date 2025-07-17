8677402af5300b51eb1380e597fd16d51d719788
Thenemex <tnmx777@gmail.com>
2025-07-17 08:53:20 +0200

    Adding new class for loading integrations for Thaumic Bases addon

--- src/main/java/thaumcraft4patched/model/config/ConfigIntegrations.java
+++ src/main/java/thaumcraft4patched/model/config/ConfigIntegrations.java
@@ -1,8 +1,16 @@
 package thaumcraft4patched.model.config;
 
+import thaumcraft4patched.config.Config;
+import thaumcraft4patched.model.integrations.ThaumicBasesCompat;
+
+import static thaumcraft4patched.api.integrations.ACompat.modIsLoaded;
+
 public class ConfigIntegrations {
 
-    public static void init() {
+    public static String tb = "thaumicbases";
 
+    public static void init() {
+        if (modIsLoaded(tb, Config.tbEnabled))
+            new ThaumicBasesCompat(tb);
     }
 }
---
+++ src/main/java/thaumcraft4patched/model/integrations/ThaumicBasesCompat.java
@@ -0,0 +1,15 @@
+package thaumcraft4patched.model.integrations;
+
+import thaumcraft4patched.api.integrations.ACompat;
+
+public class ThaumicBasesCompat extends ACompat {
+
+    public ThaumicBasesCompat(String mod) {
+        super(mod);
+    }
+
+    @Override
+    public void loadIntegration() {
+
+    }
+}
