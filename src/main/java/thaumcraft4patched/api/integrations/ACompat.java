84f467bfed1b11091825e0c34ec3d236bf3aa06b
Thenemex <tnmx777@gmail.com>
2025-07-17 08:41:23 +0200

    Added ACompat class for integrations

---
+++ src/main/java/thaumcraft4patched/api/integrations/ACompat.java
@@ -0,0 +1,45 @@
+package thaumcraft4patched.api.integrations;
+
+import cpw.mods.fml.common.Loader;
+import thaumcraft4patched.api.util.Logger;
+import thaumcraft4patched.api.util.exceptions.ParameterIsNullOrEmpty;
+
+import java.util.Objects;
+
+public abstract class ACompat {
+
+    protected final String mod;
+
+    public ACompat(String mod) {
+        if (mod == null) throw new ParameterIsNullOrEmpty();
+        this.mod = mod;
+        init();
+        Logger.logInfo("Successfully loaded integration for mod : ".concat(mod));
+    }
+
+    protected void init() {
+        loadIntegration();
+    }
+
+    public void loadIntegration() {}
+
+    public static boolean modIsLoaded(String mod, boolean config) {
+        return Loader.isModLoaded(mod) && config;
+    }
+
+    @Override
+    public String toString() {
+        return "Compat:".concat(mod);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ACompat)) return false;
+        ACompat aCompat = (ACompat) o;
+        return Objects.equals(mod, aCompat.mod);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(mod);
+    }
+}
