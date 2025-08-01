de32982825fe56e27423a5aced09efebad8f3f5e
Thenemex <tnmx777@gmail.com>
2025-08-01 21:11:58 +0200

    Added config option for disabling specific patch

--- src/main/java/thaumcraft4patched/config/Config.java
+++ src/main/java/thaumcraft4patched/config/Config.java
@@ -9,23 +9,25 @@ public class Config extends AConfig {
 
     public static boolean tc4Enabled, boneBowResearchPatchEnabled;
     public static boolean tbEnabled, missingPrereqs_ThaumiumBracelet, missingPrereqs_VoidBracelet, missingPrereqs_VoidWandCore;
-    public static boolean wgEnabled;
+    public static boolean wgEnabled, missingPrereqs_WitchingWearables;
 
     public Config(FMLPreInitializationEvent event, String version) {
         super(event, modName, modName, version);
     }
 
     protected void loadConfig() {
-        String mods = "Mods", tc4 = "Thaumcraft-4", tb = "Thaumic-Bases";
+        String mods = "Mods", tc4 = "Thaumcraft-4", tb = "Thaumic-Bases", wg = "Witching-Gadgets";
         config.addCustomCategoryComment(mods, "You can turn off bug-patches for whole mods here");
         tc4Enabled = newEntry(mods, "Thaumcraft 4");
         tbEnabled = newEntry(mods, "Thaumic Bases");
         wgEnabled = newEntry(mods, "Witching Gadgets");
-        config.addCustomCategoryComment(tc4, "You can disable/enable bug patches from Thaumcraft 4 here");
+        config.addCustomCategoryComment(tc4, "You can disable/enable bug patches for Thaumcraft 4 here");
         boneBowResearchPatchEnabled = newEntry(tc4,"HiddenBoneBowResearch", "Removes the hidden property of the research -> it will be unlocked when the player discover the Telum aspect");
-        config.addCustomCategoryComment(tb, "You can disable/enable bug patches from Thaumic Bases addon here");
+        config.addCustomCategoryComment(tb, "You can disable/enable bug patches for Thaumic Bases addon here");
         missingPrereqs_ThaumiumBracelet = newEntry(tb,"MissingPrereqs_ThaumiumBracelet", "Adds the missing prereq(s) for the \"Thaumium Bracelet\" research");
         missingPrereqs_VoidBracelet = newEntry(tb,"MissingPrereqs_VoidBracelet", "Adds the missing prereq(s) for the \"Void Bracelet\" research");
         missingPrereqs_VoidWandCore = newEntry(tb, "MissingPrereqs_VoidWandCore", "Adds the missing prereq(s) for the \"Void Wand Core\" research");
+        config.addCustomCategoryComment(wg, "You can disable/enable bug patches for Witching Gadgets addon here");
+        missingPrereqs_WitchingWearables = newEntry(wg, "MissingPrereqs_WitchingWearables", "Adds the missing prereq(s) for the \"Witching Wearables\" research");
     }
 }
