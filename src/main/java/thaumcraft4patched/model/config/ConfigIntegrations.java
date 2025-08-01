a47f05baf6269b633953d4001743c8bc77e372cd
Thenemex <tnmx777@gmail.com>
2025-08-01 20:56:41 +0200

    Adding class for integration with Witching Gadgets

--- src/main/java/thaumcraft4patched/model/config/ConfigIntegrations.java
+++ src/main/java/thaumcraft4patched/model/config/ConfigIntegrations.java
@@ -3,14 +3,17 @@ package thaumcraft4patched.model.config;
 import cpw.mods.fml.common.Loader;
 import thaumcraft4patched.config.Config;
 import thaumcraft4patched.model.integrations.ThaumicBasesCompat;
+import thaumcraft4patched.model.integrations.WitchingGadgetsCompat;
 
 public class ConfigIntegrations {
 
-    public static final String tb = "thaumicbases";
+    public static final String tb = "thaumicbases", wg = "WitchingGadgets";
 
     public static void init() {
         if (modIsLoaded(tb, Config.tbEnabled))
             new ThaumicBasesCompat(tb);
+        if (modIsLoaded(wg,Config.wgEnabled))
+            new WitchingGadgetsCompat(wg);
     }
 
     public static boolean modIsLoaded(String mod, boolean config) {
---
+++ src/main/java/thaumcraft4patched/model/integrations/WitchingGadgetsCompat.java
@@ -0,0 +1,15 @@
+package thaumcraft4patched.model.integrations;
+
+import nemexlib.api.integrations.ACompat;
+
+public class WitchingGadgetsCompat extends ACompat {
+
+    public WitchingGadgetsCompat(String mod) {
+        super(mod);
+    }
+
+    @Override
+    public void loadIntegration() {
+
+    }
+}
