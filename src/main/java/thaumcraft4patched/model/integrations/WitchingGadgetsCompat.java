4c9cd07c38098d466b2fe041aee32f307ade1595
Thenemex <tnmx777@gmail.com>
2025-08-02 21:19:40 +0200

    Added missing prereqs for WG Witching Wearables

--- src/main/java/thaumcraft4patched/model/integrations/WitchingGadgetsCompat.java
+++ src/main/java/thaumcraft4patched/model/integrations/WitchingGadgetsCompat.java
@@ -1,6 +1,8 @@
 package thaumcraft4patched.model.integrations;
 
 import nemexlib.api.integrations.ACompat;
+import nemexlib.api.thaumcraft.API;
+import thaumcraft4patched.config.Config;
 
 public class WitchingGadgetsCompat extends ACompat {
 
@@ -10,6 +12,11 @@ public class WitchingGadgetsCompat extends ACompat {
 
     @Override
     public void loadIntegration() {
+        if (Config.missingPrereqs_WitchingWearables) addMissingPrereqForWitchingWearables();
+    }
 
+    public void addMissingPrereqForWitchingWearables() {
+        // Infusion, Primal Arrows
+        API.addParents("WITCHGADG", "WGBAUBLES", true, "INFUSION", "PRIMALARROW");
     }
 }
