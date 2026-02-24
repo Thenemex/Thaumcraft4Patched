5d41942b39710231c03278718a87365d082d5118
Thenemex <tnmx777@gmail.com>
2026-02-24 16:29:43 +0100

    Adding new patch for Black Floating Candle recipe crash

--- src/main/java/thaumcraft4patched/model/config/ConfigBugPatches.java
+++ src/main/java/thaumcraft4patched/model/config/ConfigBugPatches.java
@@ -1,13 +1,20 @@
 package thaumcraft4patched.model.config;
 
+import nemexlib.api.items.ItemFinder;
+import nemexlib.api.recipes.arcane.ArcaneAdder;
 import nemexlib.api.thaumcraft.API;
+import nemexlib.api.thaumcraft.aspects.Aspects;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import thaumcraft.api.ThaumcraftApi;
 import thaumcraft.api.crafting.InfusionRecipe;
+import thaumcraft.api.crafting.ShapelessArcaneRecipe;
 import thaumcraft.api.research.ResearchItem;
+import thaumcraft.common.config.ConfigItems;
 import thaumcraft4patched.model.patch.FakePlayerPatch;
 import thaumcraft4patched.model.patch.IPatch;
 
+import static thaumcraft.api.aspects.Aspect.AIR;
 import static thaumcraft.api.aspects.Aspect.WEAPON;
 import static thaumcraft4patched.Thaumcraft4Patched.logger;
 import static thaumcraft4patched.config.Config.*;
@@ -23,6 +30,7 @@ public class ConfigBugPatches {
 
     public static void initTX() {
         if (removeNecroInfusionRecipe) removeNecroInfusionRecipe();
+        if (blackFloatingCandleRecipePatchEnabled) patchBlackFloatingCandleRecipe();
     }
 
     protected static void patchHiddenBoneBowResearch() {
@@ -48,6 +56,33 @@ public class ConfigBugPatches {
             ThaumcraftApi.getCraftingRecipes().remove(toRemove);
             logger.info("Successfully removed infusion recipe linked to \"NECROINFUSION\" tag !");
         } else
-            logger.info("Could not remove of find infusion recipe linked to \"NECROINFUSION\" tag ...");
+            logger.info("Could not remove or find infusion recipe linked to \"NECROINFUSION\" tag ...");
+    }
+
+    protected static void patchBlackFloatingCandleRecipe() {
+        ShapelessArcaneRecipe recipeToRemove = null;
+        String researchTag = "FLOATCANDLE";
+        for (Object o : ThaumcraftApi.getCraftingRecipes())
+            if (o instanceof ShapelessArcaneRecipe) {
+                ShapelessArcaneRecipe recipe = (ShapelessArcaneRecipe) o;
+                if (recipe.research.equals(researchTag) && recipe.getRecipeOutput().getItemDamage() == 32767)
+                    recipeToRemove = recipe;
+            }
+        if (recipeToRemove == null)
+            logger.info("Could not patch the recipe for Black Floating Candle / Report to Author");
+        else {
+            ItemStack blackFloatingCandleTX = ItemFinder.findItem("ThaumicExploration", "floatCandle", 15),
+                      blackCandle = ItemFinder.findItemTC("blockCandle", 15);
+            blackFloatingCandleTX.stackSize = 3;
+            ThaumcraftApi.getCraftingRecipes().remove(recipeToRemove);
+            ArcaneAdder.addArcane(
+                    researchTag,
+                    new Aspects(AIR, 5),
+                    true,
+                    false,
+                    blackFloatingCandleTX,
+                    blackCandle, blackCandle, blackCandle, ConfigItems.itemShard);
+            logger.info("Successfully patched the recipe for Black Floating Candle [TX] !");
+        }
     }
 }
