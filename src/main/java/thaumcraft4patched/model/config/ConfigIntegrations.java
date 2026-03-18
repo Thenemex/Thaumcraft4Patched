28766ea4a0dc1bb0d226b54f6073e5c356690540
Thenemex <tnmx777@gmail.com>
2026-03-18 01:33:59 +0100

    Replaced modIsLoaded() method with the one already present in NemexLib.ACompat

--- src/main/java/thaumcraft4patched/model/config/ConfigIntegrations.java
+++ src/main/java/thaumcraft4patched/model/config/ConfigIntegrations.java
@@ -1,22 +1,19 @@
 package thaumcraft4patched.model.config;
 
-import cpw.mods.fml.common.Loader;
 import thaumcraft4patched.config.Config;
 import thaumcraft4patched.model.integrations.ThaumicBasesCompat;
 import thaumcraft4patched.model.integrations.WitchingGadgetsCompat;
+
+import static nemexlib.api.integrations.ACompat.isModLoaded;
 
 public class ConfigIntegrations {
 
     public static final String tb = "thaumicbases", wg = "WitchingGadgets";
 
     public static void init() {
-        if (modIsLoaded(tb, Config.tbEnabled))
+        if (isModLoaded(tb, Config.tbEnabled))
             new ThaumicBasesCompat(tb);
-        if (modIsLoaded(wg, Config.wgEnabled))
+        if (isModLoaded(wg, Config.wgEnabled))
             new WitchingGadgetsCompat(wg);
-    }
-
-    public static boolean modIsLoaded(String mod, boolean config) {
-        return Loader.isModLoaded(mod) && config;
     }
 }
