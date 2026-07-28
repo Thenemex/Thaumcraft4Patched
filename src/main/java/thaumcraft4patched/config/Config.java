94de15dc494508be6b3e8f5d45d530ad33a9f5bc
Thenemex <tnmx777@gmail.com>
2026-07-28 19:10:56 +0200

    Added new config entries Refactor code

--- src/main/java/thaumcraft4patched/config/Config.java
+++ src/main/java/thaumcraft4patched/config/Config.java
@@ -7,6 +7,7 @@ import static thaumcraft4patched.Thaumcraft4Patched.modName;
 
 public class Config extends AConfig {
 
+    public static boolean mcEnabled, opaqueFogNetherDarkShrineJava25PatchEnabled;
     public static boolean tc4Enabled, boneBowResearchPatchEnabled, golemLumberCoreWoodHardnessPatchEnabled, nullResearchParentsPatchEnabled;
     public static boolean tbEnabled, missingPrereqs_ThaumiumBracelet, missingPrereqs_VoidBracelet, missingPrereqs_VoidWandCore;
     public static boolean wgEnabled, missingPrereqs_WitchingWearables;
@@ -17,24 +18,27 @@ public class Config extends AConfig {
     }
 
     protected void loadConfig() {
-        String mods = "Mods", tc4 = "Thaumcraft-4", tb = "Thaumic-Bases", tx = "Thaumic-Exploration", wg = "Witching-Gadgets";
-        config.addCustomCategoryComment(mods, "You can turn off bug-patches for whole mods here");
+        String mods = "Mods", mc = "Magic Cookies", tc4 = "Thaumcraft-4", tb = "Thaumic-Bases", tx = "Thaumic-Exploration", wg = "Witching-Gadgets";
+        comment(mods, "You can turn off bug-patches for whole mods here");
+        mcEnabled = newEntry(mods, "Magic Cookies");
         tc4Enabled = newEntry(mods, "Thaumcraft 4");
         tbEnabled = newEntry(mods, "Thaumic Bases");
         txEnabled = newEntry(mods, "Thaumic Exploration");
         wgEnabled = newEntry(mods, "Witching Gadgets");
-        config.addCustomCategoryComment(tc4, "You can disable/enable bug patches for Thaumcraft 4 here");
+        comment(mc, "You can disable/enable bug patches for Magic Cookies here");
+        opaqueFogNetherDarkShrineJava25PatchEnabled = newEntry(mc, "OpaqueFogNetherDarkShrineJava25", "Removes the opaque fog inside the Dark Shrine, when using higher Java version");
+        comment(tc4, "You can disable/enable bug patches for Thaumcraft 4 here");
         boneBowResearchPatchEnabled = newEntry(tc4,"HiddenBoneBowResearch", "Removes the hidden property of the research -> it will be unlocked when the player discover the Telum aspect");
         golemLumberCoreWoodHardnessPatchEnabled = newEntry(tc4, "GolemLumberBlockHardness", "This patches the issue when Golem with Lumber core cannot drop wood blocks that are too hard to be broken by hand (compatible with HLC)");
         nullResearchParentsPatchEnabled = newEntry(tc4, "NullResearchParentsPatchedEnabled", false, "Special tweak that will replace any parents/parentsHidden that are Null with an empty array, iterating on all researches registered");
-        config.addCustomCategoryComment(tb, "You can disable/enable bug patches for Thaumic Bases addon here");
+        comment(tb, "You can disable/enable bug patches for Thaumcraft 4 here");
         missingPrereqs_ThaumiumBracelet = newEntry(tb,"MissingPrereqs_ThaumiumBracelet", "Adds the missing prereq(s) for the \"Thaumium Bracelet\" research");
         missingPrereqs_VoidBracelet = newEntry(tb,"MissingPrereqs_VoidBracelet", "Adds the missing prereq(s) for the \"Void Bracelet\" research");
         missingPrereqs_VoidWandCore = newEntry(tb, "MissingPrereqs_VoidWandCore", "Adds the missing prereq(s) for the \"Void Wand Core\" research");
-        config.addCustomCategoryComment(tx, "Thaumic Exploration");
+        comment(tx, "You can disable/enable bug patches for Thaumic Exploration here");
         blackFloatingCandleRecipePatchEnabled = newEntry(tx, "BlackFloatingCandle", "This patches the crash caused by trying to craft a Black Floating Candle");
         removeNecroInfusionRecipe = newEntry(tx, "RemoveNecroInfusionRecipe", "Remove the buggy infusion recipe for the \"NecroAltar\" with unregistered/null output itemAlter");
-        config.addCustomCategoryComment(wg, "You can disable/enable bug patches for Witching Gadgets addon here");
+        comment(wg, "You can disable/enable bug patches for Witching Gadgets addon here");
         missingPrereqs_WitchingWearables = newEntry(wg, "MissingPrereqs_WitchingWearables", "Adds the missing prereq(s) for the \"Witching Wearables\" research");
     }
 }
